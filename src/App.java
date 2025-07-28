ec35d96ebfd0eab513ca53b642d7ff5d665d514f
Mubtasim Sajid Ahmed Arian <mubtasimsajidahmedarian.11@gmail.com>
2025-07-28 21:57:13 +0600

    Initial Set-up

--- src/App.java
+++ src/App.java
@@ -1,4 +1,8 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -6,6 +10,13 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         try {
+            Parent root = FXMLLoader.load(getClass().getResource("client/chat_ui.fxml"));
+            Scene scene = new Scene(root);
+            Image tongLogo = new Image("/resources/tongLogo.png");
+
+            stage.setTitle("Tong");
+            stage.getIcons().add(tongLogo);
+            stage.setScene(scene);
             stage.show();
         } catch (Exception e) {
             e.printStackTrace();
